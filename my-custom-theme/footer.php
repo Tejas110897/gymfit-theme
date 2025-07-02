2574e299d1e645313a5159e72c61ea64a7ed903d
Tejas S <tejas.s@criticalog.com>
2025-07-02 22:13:34 +0530

    deleted

--- my-custom-theme/footer.php
+++
@@ -1,6 +0,0 @@
-  <footer class="section">
-    <p>&copy; <?php echo date('Y'); ?> GymFit. All rights reserved.</p>
-  </footer>
-  <?php wp_footer(); ?>
-</body>
-</html>
--- my-custom-theme/template-parts/content-testimonial.php
+++
@@ -1,4 +0,0 @@
-<article class="testimonial">
-    <h3><?php the_title(); ?></h3>
-    <p><?php the_content(); ?></p>
-</article>
